fa01bc16ed504e143edb9dc538fa1b1b9f06c7ad
Daniel-Pacer <daniel-pacer@Daniel-Pacers-MacBook-Pro.local>
2023-12-05 11:54:01 +0800

    update skill and fix language padding space in between

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,18 +1,26 @@
-import { Flex, Progress, Text } from "@chakra-ui/react";
+import { Flex, Progress, Text, Container } from "@chakra-ui/react";
 import { NextComponentType } from "next";
 
 type Props = {
-    language: string;
-    level: number;
-}
+  language: string;
+  level: number;
+};
 
 const Skill: React.FC<Props> = ({ language, level }) => {
-    return (
-        <Flex flexDirection="row" justify="left" align="center" gap={8} mt={4}>
-            <Text>{language}</Text>
-            <Progress value={level} colorScheme="purple" width="100%" outline="1px solid lightgray" rounded={8} />
-        </Flex>
-    );
-}
+  return (
+    <Flex flexDirection="row" justify="left" align="center" mt={4}>
+      <Container maxW={"150px"}>
+        <Text>{language}</Text>
+      </Container>
+      <Progress
+        value={level}
+        colorScheme="purple"
+        width="100%"
+        outline="1px solid lightgray"
+        rounded={8}
+      />
+    </Flex>
+  );
+};
 
 export default Skill;
